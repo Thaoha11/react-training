e57946807c248823c83641c67786264ad1617535
Thaoha11 <thaohaexo@gmail.com>
2022-06-28 15:55:25 +0700

    add example for useContext

---
+++ example/src/store/actions.js
@@ -0,0 +1,11 @@
+import { SET_TODO_IPNUT, ADD_TODO } from "./constants";
+
+export const setTodoInput = payload => ({
+    type: SET_TODO_IPNUT,
+    payload
+})
+
+export const addTodo = payload => ({
+    type: ADD_TODO,
+    payload
+})
---
+++ example/src/store/reducer.js
@@ -0,0 +1,27 @@
+import { SET_TODO_IPNUT, ADD_TODO } from './constants'
+
+const initState = {
+    todos: [],
+    todoInput: ''
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case SET_TODO_IPNUT:
+            return {
+                ...state,
+                todoInput: action.payload
+            }
+        case ADD_TODO:
+            return {
+                ...state,
+                todos: [...state.todos, action.payload],
+                todoInput: ''
+            }
+        default:
+            throw new Error('Invalid action')
+    }
+}
+
+export { initState }
+export default reducer
