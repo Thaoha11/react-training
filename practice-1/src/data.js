7300a98c2b898fa5b454d7d54cad7c157557acce
Thaoha11 <thaohaexo@gmail.com>
2022-07-22 15:44:49 +0700

    Add data

---
+++ practice-1/src/data.js
@@ -0,0 +1,18 @@
+
+let DSproduct = [
+    {
+        productId: '1',
+        productName: 'Bucket',
+        productPrice: '100$',
+        productBrand: 'MLB',
+        productImage: 'https://product.hstatic.net/200000384787/product/ucket_mlb_monogram_diamond_jacquard_boston_red_sox_3ahtm111n-43bgd__7__5afd7a13eab44e139f75a6bb4628544d_master.jpg'
+    },
+    {
+        productId: '2',
+        productName: 'Bag',
+        productPrice: '200$',
+        productBrand: 'C&K',
+        productImage: 'https://www.charleskeith.com/on/demandware.static/-/Library-Sites-CharlesKeith/default/dw8ab22d4a/images/Muse/Spring22/krystal/blooming-spring/charles-keith-spring-22-blooming-spring-with-krystal-blog-02.jpg'
+    }
+]
+export { DSproduct } 
